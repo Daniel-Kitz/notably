fd90f76d894b45ca34e6489f1a6feba32bdeee32
LordQuantumKeks <lordquantumkeks@gmail.com>
2021-04-15 22:43:05 +0200

    Added Replacer and Timetable logic

--- js/timetable.js
+++ js/timetable.js
@@ -6,6 +6,7 @@ auth.onAuthStateChanged(user => {
     if (user) {
         db.collection('timetables').get().then(snapshot => {
             timetablesetup(snapshot.docs)
+            test();
         })
     } else {
         timetablesetup([ ])
@@ -23,20 +24,44 @@ const timetablesetup = (data) => {
             timetable = item
         }
 
-        console.log(timetable);
     })
 
 }
 
-var tablehead = pagetable.children[0];
+function test() {
 
-console.log(tablehead.childElementCount);
+    var tablehead = pagetable.children[0];
 
-for (var n = 1; n < 6; n++) {
-    for (var i = 1; i < tablehead.childElementCount; i++) {
+    for (var n = 1; n < 6; n++) {
 
-        console.log(tablehead.children[i].children[n]);
-    
+        for (var i = 1; i < tablehead.childElementCount; i++) {
+
+            let current = tablehead.children[i].children[n].children[0]
+
+            if (current != undefined) {
+
+                switch (n) {
+
+                    case 0:
+                        current.children[0].children[0].innerHTML = 'monday';
+                        current.children[0].children[1].innerHTML = 'monday';
+                        current.children[0].children[2].innerHTML = 'monday';
+                    case 1:
+                        current.children[0].children[0].innerHTML = 'tuesday';
+                        current.children[0].children[1].innerHTML = 'tuesday';
+                        current.children[0].children[2].innerHTML = 'tuesday';
+                }
+
+                current.classList.remove('invis')
+
+                console.log(tablehead.children[i].children[n])
+            }
+
+            // console.log(timetable['monday']);
+            //console.log(current);
+
+        }
     }
 }
+
  
